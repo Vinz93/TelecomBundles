e64279b14776f78247a0a58608e5f5c2f9baa5db
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2018-03-02 11:54:47 -0400

    handle bundle prices

--- server/src/graph_functions.js
+++ server/src/graph_functions.js
@@ -2,18 +2,30 @@ import graph from './graph';
 const { vertices, edges } = graph;
 
 let combinations = [];
+let prices = [];
 
-function printCombinations(combinations) {
+function bundlePrice(bundle) {
+  return bundle.reduce((acum, vertex, i, bundle) => {
+    const [nextVertex, vertexPrice] = [bundle[i + 1], vertex.price || 0];
+    let edgeCost = 0;
+    if (!!nextVertex) {
+      edgeCost = edges[vertex.id].find(edge => nextVertex.id === edge[0])[1];
+    }
+    return acum + vertexPrice + edgeCost;
+  }, 0);
+}
+function printCombinations(combinations, prices) {
   combinations.forEach((bundle, i) => {
-    console.log(`bundle ${i})
-    `);
     console.log(bundle);
-    console.log(`========`);
+    console.log(`total = ${prices[i]}`);
+    console.log(` 
+      `);
   });
 }
 
 function vertexCombination(vertex, stack) {
-  let stackAux = [...stack, vertex.name];
+  let stackAux = [...stack, vertex];
+  prices = [...prices, bundlePrice(stackAux)];
   combinations = [...combinations, stackAux];
   for (const i in edges[vertex.id]) {
     vertexCombination(vertices[edges[vertex.id][i][0]], stackAux);
@@ -21,11 +33,10 @@ function vertexCombination(vertex, stack) {
   stackAux = [...stack.slice(0, stack.lenght)];
 }
 
-// vertexCombination(vertices[3], []);
 function graphCombinations() {
   vertices.forEach(vertice => {
     vertexCombination(vertice, []);
   });
 }
 graphCombinations();
-printCombinations(combinations);
+printCombinations(combinations, prices);
