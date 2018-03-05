5b40645e12618df9844544bb5184a531d1a86f03
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2018-03-05 12:17:01 -0400

    frontend details

--- client/src/components/Plan.jsx
+++ client/src/components/Plan.jsx
@@ -1,12 +1,11 @@
 import React, { PropTypes } from 'react';
 const Plan = ({ plan }) => {
-  const services = plan.map(({ name, id, price = 0 }) => {
-    return (
-      <li key={id} className="service">
-        {name} <span className="currency">{price}$</span>
-      </li>
-    );
-  });
+  const services = plan.map(({ name, id, price = 0 }) => (
+    <li key={id} className="service">
+      {name} <span className="currency">{price}$</span>
+    </li>
+  ));
+
   return (
     <ul className="plan">
       {services}
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,4 @@
-import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import BundleList from './BundleList.jsx';
 
@@ -8,36 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="main-app-container">
-        <div className="main-app-nav">Bundles combinations</div>
+        <div className="main-app-nav">Telecom bundles combinations</div>
         <BundleList />
         <Footer />
       </div>
     );
   }
 }
-
-/**
- * Keep in mind that 'state' isn't the state of local object, but your single
- * state in this Redux application. 'bundles' is a property within our store/state
- * object. By mapping it to props, we can pass it to the child component Counter.
- */
-
-/**
- * Turns an object whose values are 'action creators' into an object with the same
- * keys but with every action creator wrapped into a 'dispatch' call that we can invoke
- * directly later on. Here we imported the actions specified in 'CounterActions.js' and
- * used the bindActionCreators function Redux provides us.
- *
- * More info: http://redux.js.org/docs/api/bindActionCreators.html
- */
-
-
-/**
- * 'connect' is provided to us by the bindings offered by 'react-redux'. It simply
- * connects a React component to a Redux store. It never modifies the component class
- * that is passed into it, it actually returns a new connected componet class for use.
- *
- * More info: https://github.com/rackt/react-redux
- */
 
 export default App;
